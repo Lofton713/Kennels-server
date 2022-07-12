939540ad5f6d0c64c456a79043b8e08503876d33
Stacy Lofton <lofton713@gmail.com>
2022-07-12 11:36:02 -0500

    chapter 5 working

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,11 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from views import ( get_all_animals, get_all_customers, get_all_employees,get_all_locations,
+                   get_single_animal,get_single_customer,get_single_employee,get_single_location,
+                   create_customer,create_employee,create_location,delete_animal,delete_location,
+                   delete_employee, delete_customer)
 
-from views.animal_requests import create_animal, get_all_animals, get_single_animal
-from views.customer_requests import create_customer, get_all_customers, get_single_customer
-from views.employee_requests import create_employee, get_all_employees, get_single_employee
-from views.location_requests import create_location, get_all_locations, get_single_location
+
 
 
 # Here's a class. It inherits from another class.
@@ -155,6 +156,29 @@ class HandleRequests(BaseHTTPRequestHandler):
         """Handles PUT requests to the server
         """
         self.do_POST()
+    
+    def do_DELETE(self):
+    # Set a 204 response code
+        self._set_headers(204)
+
+    # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+    # Delete a single animal from the list
+        if resource == "locations":
+            delete_location(id)
+    # Delete a single animal from the list
+        if resource == "customers":
+            delete_customer(id)
+    # Delete a single animal from the list
+        if resource == "employees":
+            delete_employee(id)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
